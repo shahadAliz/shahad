412c366ad2494f7ccabc2c56c4141a0a3f383d97
shahadAliz <cs.shahadz@gmail.com>
2024-07-23 20:45:42 +0300

    first commit

---
+++ shahadd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shahadd
+//
+//  Created by شهد علي on 10/01/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shahadd/shahaddApp.swift
@@ -0,0 +1,17 @@
+//
+//  shahaddApp.swift
+//  shahadd
+//
+//  Created by شهد علي on 10/01/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct shahaddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
